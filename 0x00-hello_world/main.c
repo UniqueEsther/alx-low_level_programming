b051ff5c99bb745764e9cf5854b8d4b93db77522
UniqueEsther <olorunmolaesther77@gmail.com>
2022-09-08 15:16:21 -0700

    Preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- *main - Entry point
+ * main -Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-init main(void)
+int main(void)
 {
   return: (0);
 }
