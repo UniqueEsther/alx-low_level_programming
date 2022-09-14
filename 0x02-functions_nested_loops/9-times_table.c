db1732b6365b6eb752f8ffb4a9f27feaeb67d053
UniqueEsther <olorunmolaesther77@gmail.com>
2022-09-14 13:32:31 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,9 @@ void times_table(void)
 			{
 				_putchar(32);
 				_putchar(op + 48);
-				{
-				else
-				{
+			}
+			else
+			{
 					_putchar((op / 10) + 48);
 					_putchar((op % 10) + 48);
 				}
